04e413e4b79d51097e29dda9b467ab0670542814
yixinxiang <37011596+yixinxiang@users.noreply.github.com>
2019-03-31 12:54:24 -0400

    Fixed errors with null and default

--- models.py
+++ models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
 class Students(models.Models):
-    student_id = models.IntegerField(primary_key = TRUE)
+    student_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     discipline = models.CharField(max_length=200)
     year = models.CharField(max_length=200)
     phone = models.IntegerField(Default=0)
-    interview_offer = models.BooleanField(null=TRUE, default=NULL)
-    project_name = models.ForeignKey(Projects, on_delete=models.CASCADE, default=NULL)
+    //interview_offer = models.BooleanField(null=True, default=None)
+    //project_name = models.ForeignKey(Projects, on_delete=models.CASCADE, default=None)
+    interview_offer = models.BooleanField(null=True)
+    project_name = models.ForeignKey(Projects, on_delete=models.CASCADE)
 
 class Projects(models.Models):
-    project_name = models.CharField(max_length=200, primary_key = TRUE)
+    project_name = models.CharField(max_length=200, primary_key=True)
     not_for_profit_name = models.ForeignKey(notForProfit, on_delete=models.CASCADE)
 
 class notForProfit(models.Models):
-    not_for_profit_name.CharField(max_length=200, primary_key = TRUE)
+    not_for_profit_name.CharField(max_length=200, primary_key=True)
     email = models.CharField(max_length=200)
     project_name = models.ForeignKey(Projects, on_delete=models.CASCADE)
 
@@ -23,6 +25,6 @@ class notForProfit(models.Models):
         unique_together = (('not_for_profit_name', 'project_name'),)
 
 class Teams(models.Models):
-    team_member_id = models.IntegerField(primary_key = TRUE)
-    project_name = models.ForeignKey(Projects, on_delete = CASCADE)
-    student_id = models.ForeignKey(Students, on_delete = CASCADE)
+    team_member_id = models.IntegerField(primary_key=True)
+    project_name = models.ForeignKey(Projects, on_delete = models.CASCADE)
+    student_id = models.ForeignKey(Students, on_delete = models.CASCADE)
